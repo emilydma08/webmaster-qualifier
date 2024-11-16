4ed6bb09e37af6e311145ade2f56b11f9711e087
emilydma08 <142754713+emilydma08@users.noreply.github.com>
2024-11-16 09:38:00 -0800

    Added files and navigation bar

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,9 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 #Hello
